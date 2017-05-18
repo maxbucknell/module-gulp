66c81bb1d24ee703b9f74d99714bd4609e80d37a
Max Bucknell <me@maxbucknell.com>
2017-05-18 15:33:50 +0100

    Clean requirejs-config.js

--- gulp/task/clean.js
+++ gulp/task/clean.js
@@ -3,6 +3,7 @@ const rimraf = require('rimraf');
 function main (config) {
     rimraf.sync(config.build_dir);
     rimraf.sync(config.output_dir);
+    rimraf.sync(config.requirejs_config_dir);
 }
 
 module.exports = main;
