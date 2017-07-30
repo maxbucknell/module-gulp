2e101ae36f0aca03039e97cb85fa4376c36916af
Max Bucknell <me@maxbucknell.com>
2017-07-30 18:04:53 +0100

    Add copy task

---
+++ gulp/task/copy.js
@@ -0,0 +1,27 @@
+const fs = require('fs-extra');
+const path = require('path');
+const magentoData = require('MaxBucknell_Gulp/lib/magento-data');
+const _ = require('lodash');
+
+const validExtensions = _.map(
+    process.argv.slice(2),
+    (ext) => `.${ext}`
+);
+
+function copy () {
+    console.log(magentoData.build_dir, magentoData.output_dir);
+    fs.copy(
+        path.join(magentoData.build_dir, 'flat/static'),
+        magentoData.output_dir,
+        {
+            dereference: true,
+            filter: (src) => {
+                const ext = path.extname(src);
+
+                return !(ext.length) || _.includes(validExtensions, ext);
+            }
+        }
+    )
+}
+
+copy();
